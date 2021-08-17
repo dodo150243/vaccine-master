3652e95a5d9d77ba891344023cd95b443ab06be5
dodo150243 <tirawat330@gmail.com>
2021-08-17 15:04:53 +0700

    Initial commit

---
+++ src/components/Vaccine.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Vaccine.css';
+import VacImage from './VacImage';
+
+class Vaccine extends React.Component {
+    constructor(props){
+      super(props);
+      this.state={
+        short : '1000000',
+        have: true,
+      }
+    }
+
+  render() {
+    if (!this.state.have){
+      return <div>Not found</div>;
+      
+    }
+   
+
+    return (
+      <div className="vaccine__list">
+        <VacImage path="/img/1.jpg" />
+        <div className="vaccine__item">{this.props.nameCompony}</div>
+        <div className="vaccine__item">{this.props.nameInter}</div>
+        <div className="vaccine__item">{this.props.produce}</div>
+        <div className="vaccine__item">{this.props.technology}</div>
+        <div>{this.state.short}</div>
+        <div>{`${this.state.have}`}</div>
+      </div>
+    );
+  }
+}
+
+// export const Vac = Vaccine;
+export default Vaccine;
